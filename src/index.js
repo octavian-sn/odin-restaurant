3a962d0a11d5c27c43f3a3def0f7b76213a30d2c
Sulic Octavian <octavius1992@gmail.com>
2022-10-18 12:41:54 +0300

    Add menu button functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import home from './home.js';
 import { body, homePage } from './home.js';
+import menuPage from './menu';
 
 const display = document.getElementById('content');
 export default display;
@@ -10,7 +11,16 @@ const content = document.getElementById('text');
 
 buttons.home.addEventListener('click', function(){
     this.classList.add('active');
+    buttons.menu.classList.remove('active');
+    buttons.contact.classList.remove('active');
     homePage(content);
+})
+
+buttons.menu.addEventListener('click', function(){
+    this.classList.add('active');
+    buttons.contact.classList.remove('active');
+    buttons.home.classList.remove('active');
+    menuPage(content);
 })
 
 
--- src/menu.js
+++ src/menu.js
@@ -1,2 +1,50 @@
+export default function menuPage(arg) {
+    arg.innerHTML = '';
 
+    const menu = document.createElement('div');
+    menu.id = 'menu';
+
+    products.forEach(item => {
+        const frame = document.createElement('div');
+        frame.classList.add('dish');
+        const title = document.createElement('h2');
+        title.textContent = item.name;
+        frame.appendChild(title);
+        const info = document.createElement('p');
+        info.textContent = item.description;
+        frame.appendChild(info);
+        const price = document.createElement('p');
+        price.textContent = item.price;
+        frame.appendChild(price);
+        const picture = document.createElement('img');
+        picture.src = item.picture;
+        frame.appendChild(picture);
+
+        menu.appendChild(frame);
+    })
+
+    arg.appendChild(menu);
+
+}
+
+const products = [
+    {
+        'name': 'Le Grand Salmon',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'picture': '../src/images/salmon.jpg',
+        'price' : '399.99$'
+    },
+    {
+        'name': 'Sizzlering Sandwich',
+        'description': 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'picture': '../src/images/sandwich.jpg',
+        'price' : '26.99$'
+    },
+    {
+        'name': 'The Perverted Pancake',
+        'description': 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'picture': '../src/images/pancake.jpg',
+        'price' : '128.99$'
+    }
+]
 
